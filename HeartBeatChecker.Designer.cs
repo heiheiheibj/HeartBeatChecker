9ee555426182dc50f7c7ed9d8f891c54336360b2
hater.li <hater@163.com>
2018-03-25 00:09:57 +0800

    submit heartbeatchecker

---
+++ HeartBeatChecker.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace HeartBeatChecker
+{
+    partial class HeartBeatChecker
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HeartBeatChecker));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSMTP = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textCC1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textCC2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textCC3 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textCC4 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtToMail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSMTPUserName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始监测";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SMTP:";
+            // 
+            // txtSMTP
+            // 
+            this.txtSMTP.Location = new System.Drawing.Point(73, 22);
+            this.txtSMTP.Name = "txtSMTP";
+            this.txtSMTP.Size = new System.Drawing.Size(144, 21);
+            this.txtSMTP.TabIndex = 2;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(73, 49);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(144, 21);
+            this.txtMail.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "发件:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(73, 73);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(144, 21);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "密码:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(227, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 21);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textCC1
+            // 
+            this.textCC1.Location = new System.Drawing.Point(73, 179);
+            this.textCC1.Name = "textCC1";
+            this.textCC1.Size = new System.Drawing.Size(144, 21);
+            this.textCC1.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "抄送1:";
+            // 
+            // textCC2
+            // 
+            this.textCC2.Location = new System.Drawing.Point(73, 207);
+            this.textCC2.Name = "textCC2";
+            this.textCC2.Size = new System.Drawing.Size(144, 21);
+            this.textCC2.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 210);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 12);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "抄送2:";
+            // 
+            // textCC3
+            // 
+            this.textCC3.Location = new System.Drawing.Point(73, 234);
+            this.textCC3.Name = "textCC3";
+            this.textCC3.Size = new System.Drawing.Size(144, 21);
+            this.textCC3.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(32, 237);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 12);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "抄送3:";
+            // 
+            // textCC4
+            // 
+            this.textCC4.Location = new System.Drawing.Point(73, 261);
+            this.textCC4.Name = "textCC4";
+            this.textCC4.Size = new System.Drawing.Size(144, 21);
+            this.textCC4.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(32, 264);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 12);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "抄送4:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "秒";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(73, 152);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(53, 21);
+            this.txtTime.TabIndex = 24;
+            this.txtTime.Text = "30";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "时间:";
+            // 
+            // txtToMail
+            // 
+            this.txtToMail.Location = new System.Drawing.Point(73, 126);
+            this.txtToMail.Name = "txtToMail";
+            this.txtToMail.Size = new System.Drawing.Size(144, 21);
+            this.txtToMail.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "收件:";
+            // 
+            // txtSMTPUserName
+            // 
+            this.txtSMTPUserName.Location = new System.Drawing.Point(73, 100);
+            this.txtSMTPUserName.Name = "txtSMTPUserName";
+            this.txtSMTPUserName.Size = new System.Drawing.Size(144, 21);
+            this.txtSMTPUserName.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "帐号:";
+            // 
+            // HeartBeatChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 347);
+            this.Controls.Add(this.txtToMail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSMTPUserName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textCC4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textCC3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textCC2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textCC1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSMTP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "HeartBeatChecker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "服务器监控";
+            this.Load += new System.EventHandler(this.HeartBeatChecker_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSMTP;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textCC1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textCC2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textCC3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textCC4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtToMail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSMTPUserName;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ HeartBeatChecker.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Text;
+using System.Windows.Forms;
+ 
+using System.Net.Mail;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+namespace HeartBeatChecker
+{
+    public partial class HeartBeatChecker : Form
+    {
+       
+        string[] arrUrl = new string[20];
+        //string[] arrUrlDesc = new string[20];
+        //string strFileContent = "";
+        public HeartBeatChecker()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtSMTP.Text.Trim().Length < 3 || txtMail.Text.Trim().Length < 3 || txtPassword.Text.Trim().Length < 3 || txtToMail.Text.Trim().Length < 3)
+            {
+                MessageBox.Show("请完整填写界面中的内容项!");
+                return;
+                
+            }
+            button1.Text = "监控中";
+            int j = ReadText();
+            if (j <=1 )
+            {
+                MessageBox.Show("在当前目录下未找到site.ini文件或文件中没有有效网址!");
+                return;
+            }
+
+
+            int intT = 1000;
+            try
+            {
+                intT = Convert.ToInt32(txtTime.Text) * intT;
+                if (intT < 3000)
+                {
+                    MessageBox.Show("最小时间间隔为3秒" + intT.ToString());
+                    txtTime.Focus();
+                    return;
+                }
+                timer1.Interval = intT;
+                StartCheck();
+            }
+            catch
+            {
+                MessageBox.Show("最小时间间隔为3秒!" + intT.ToString());
+                txtTime.Focus();
+                return;            
+            }
+            
+
+        }
+
+        private void StartCheck()
+        {
+            string v = "";
+            int intError = 0;
+            timer1.Stop();
+            for (int i = 1; i <= arrUrl.Length; i++)
+            {
+                try
+                {                    
+                    v = CheckServer(arrUrl[i].ToString());
+                    if (v != "")
+                    {
+                        intError = 1;
+                        if (CheckServer("http://www.baidu.com", "baidu") == "")
+                        {
+                            SendMail(v);
+                        }
+                        else
+                        {
+                            intError = 0;   //如果BAIDU也出错说明网络问题，不发送邮件
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            }
+            if (intError != 0)
+            {
+                timer1.Stop();
+                Thread.Sleep(3 * 60 * 1000);   //发过邮件后，过3分钟再继续测
+                //MessageBox.Show("ok");
+                StartCheck();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        private string CheckServer(string strUrlV,string strV ="")
+        {
+            string strTitle = "";
+            string strUrl="";
+            string strKey = "err";
+            try
+            {
+                /*objXmlHTTP.open("GET", strUrl, false, null, null);
+                objXmlHTTP.send("");
+                if (objXmlHTTP.readyState == 4)
+                {
+                    //MessageBox.Show(objXmlHTTP.status.ToString());
+                    //Byte[] b = (Byte[])objXmlHTTP.responseBody;
+                    //string andy = System.Text.Encoding.GetEncoding("GB2312").GetString(b).Trim();
+                    if (objXmlHTTP.status.ToString() != "200")
+                    {
+                        strTitle = strUrl + "发生 HTTP" + objXmlHTTP.status.ToString() + " 错误!";
+                    }
+                    else
+                    {
+                        if (objXmlHTTP.responseText.IndexOf("ok") < 0 && objXmlHTTP.responseText.IndexOf("title") <= 0)
+                        {
+                            strTitle = strUrl + "运行环境可能发生错误!";
+                        }
+                    }
+                }*/
+                if (strV == "") //如果是带第二个KEY参数，说明是效验BAIDU能不能通过
+                {
+                    string[] arrUrlDesc = strUrlV.Split('|');
+
+                    strUrl = arrUrlDesc[0];
+                    strV = arrUrlDesc[1];
+                }
+                else
+                {
+                    strUrl = strUrlV;
+                }
+                string a = GetSiteHtml(strUrl);
+                if (a.IndexOf(strV) <= 0)
+                        {
+                            strTitle = strUrl + "运行环境可能发生错误!";
+                            if (a.IndexOf("siteerror") > 0)
+                            {
+                                WriteFile("errlog.txt", a, "");
+                            }
+                        }
+                //objXmlHTTP.abort();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message + e.TargetSite + strUrl);
+                //SendMail(arrUrl[1], strTitle);
+                strTitle = strUrlV + " 发生异常!";
+            }
+            finally {
+                //objXmlHTTP.abort();
+            }
+            return strTitle;
+            
+        }
+
+        //获取某个页面。以返回流的编码方式，接收返回的信息
+        public string GetSiteHtml(string url)
+        {
+            //string tempurl = System.Web.HttpUtility.UrlEncode(url, System.Text.Encoding.GetEncoding("gb2312")); //url
+            string tempurl = url;
+            try
+            {
+                HttpWebRequest webr = (HttpWebRequest)WebRequest.Create(tempurl);
+                webr.Timeout = 60000;
+
+                //创建请求
+                HttpWebResponse wb = (HttpWebResponse)webr.GetResponse();
+                Stream sr = wb.GetResponseStream(); //得到返回数据流
+                Encoding encode;
+                switch (wb.CharacterSet.ToLower()) //返回数据流的编码方式
+                {
+                    case "utf-8":
+                        encode = System.Text.Encoding.GetEncoding("utf-8");
+                        break;
+                    case "gb2312":
+                        encode = System.Text.Encoding.GetEncoding("gb2312");
+                        break;
+                    default:
+                        encode = System.Text.Encoding.GetEncoding("utf-8"); //默认为中文
+                        break;
+                }
+                StreamReader sr1 = new StreamReader(sr, encode);
+                string zz = sr1.ReadToEnd(); //读取完成
+                sr1.Close();
+                wb.Close();//关闭
+                return zz;
+            }
+            catch (Exception x4)
+            {
+                return " siteerror:" + x4.Message;
+            }
+        }
+
+        private void SendMail(string strTitle)
+        {
+            if (txtSMTP.Text.Trim() != "" && txtMail.Text.Trim() != "")
+            {
+                
+                MailAddress objFrom = new MailAddress(txtMail.Text.Trim(), txtMail.Text.Trim());
+                MailAddress objTo = new MailAddress(txtToMail.Text, txtToMail.Text);
+                MailMessage objMail = new MailMessage(objFrom,objTo);
+                //objMail.CC.Add("13056870@qq.com");
+                //objMail.CC.Add("66014196@qq.com");
+                if (IsEmail(textCC1.Text))
+                {
+                    objMail.CC.Add(textCC1.Text);
+                }
+                if (IsEmail(textCC2.Text))
+                {
+                    objMail.CC.Add(textCC2.Text);
+                }
+                if (IsEmail(textCC3.Text))
+                {
+                    objMail.CC.Add(textCC3.Text);
+                }
+                if (IsEmail(textCC4.Text))
+                {
+                    objMail.CC.Add(textCC4.Text);
+                }
+                //objMail.CC.Add("guoshaobo@ciic.com.cn");
+                //objMail.CC.Add("guoshaobo@ciic.com.cn");            
+                
+                objMail.Subject = strTitle;
+                //objMail.Body=strUrl + "中好的";
+                objMail.Priority = MailPriority.High;
+                SmtpClient MySmtpClient = new SmtpClient(txtSMTP.Text,25);
+                MySmtpClient.UseDefaultCredentials = false;
+                //MySmtpClient.EnableSsl = true;
+                MySmtpClient.Credentials = new System.Net.NetworkCredential(txtSMTPUserName.Text, txtPassword.Text);
+                MySmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                MySmtpClient.EnableSsl = false;
+                try
+                {
+                    MySmtpClient.Send(objMail);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                    System.Environment.Exit(0); 
+                }
+                objMail.Dispose();
+                    
+
+            }
+        }
+
+        private int ReadText()
+        {
+            try
+            {
+                FileStream aFile = new FileStream(Application.StartupPath + "\\Site.ini", FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+                //strFileContent = sr.ReadToEnd();
+                string s = "";
+                //sr.BaseStream.Seek(0, SeekOrigin.Begin);  //读一下，看看文件内有没有内容，为下一步循环 提供判断依据  
+                //sr.ReadLine() 这里是 StreamReader的方法 可不是 console 中的~   string str = sr.ReadLine();//如果 文件有内容   while (str != null)  {      //输出字符串，str 在上面已经定义了 读入一行字符       Console.WriteLine("{0}", str);      //这里我的理解是 当输出一行后，指针移动到下一行~      //下面这句话就是 判断 指针所指这行是否有内容~      str = sr.ReadLine();}  //C#读取TXT文件之关闭文件，注意顺序，先对文件内部进行关闭，然后才是文件~  sr.Close();  fs.Close();      }  
+                int j = 1;
+                int n = 1;
+                s = sr.ReadLine();
+                while (s != null && j<20)
+                {
+                    if (((s.ToString()).ToLower()).IndexOf("http://") >= 0)
+                    {
+                        arrUrl[j] = s.ToString();
+                        j++;
+                    }
+                    n++;
+                    s = sr.ReadLine();
+                }
+                sr.Close();
+                sr.Dispose();
+                aFile.Close();
+                aFile.Dispose();
+                return j;
+                //MessageBox.Show(strFileContent);
+                //MessageBox.Show(strFileContent.IndexOf("\n").ToString());
+            }
+            catch(Exception e)
+            {
+                return -1;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadText();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            StartCheck();
+        }
+
+        //文件名，内容，放在哪个子目录下，文件内容中是否加在尾上加上时间信息
+        public void WriteFile(string strLogFileName, string LogStr, string strSiteFolder = "", bool bolNeedDateTime = true)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                if (bolNeedDateTime)
+                {
+                    LogStr = DateTime.Now.ToLocalTime().ToString() + "\n" + LogStr;
+                }
+                if (strSiteFolder != "")
+                {
+                    strLogFileName = Application.StartupPath.ToString()  + "\\" + strLogFileName;
+                }
+                sw = new StreamWriter(strLogFileName, true, Encoding.Default);
+                sw.WriteLine(LogStr);
+            }
+            catch (Exception exf)
+            {
+                string v = exf.Message.ToString();
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }
+            sw.Dispose();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool IsEmail(string str)
+        {
+            //string res = string.Empty;
+
+            string expression = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            if (System.Text.RegularExpressions.Regex.IsMatch(str, expression, System.Text.RegularExpressions.RegexOptions.Compiled))
+            {                
+                return true;
+            }
+            return false;
+        }
+
+        private void HeartBeatChecker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
